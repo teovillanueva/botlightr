c7666146bbf3f3ff424c3cad4cf25a430e7b173f
Teodoro Villanueva <teodoro2102@gmail.com>
2020-05-07 14:17:03 -0300

    Build fix

--- webpack.config.ts
+++ webpack.config.ts
@@ -29,6 +29,7 @@ export default {
     output: {
         path: path.resolve(__dirname, "dist"),
         filename: "bundle.js",
+        libraryTarget: "commonjs"
     },
     plugins: [
         new NodemonPlugin(),
